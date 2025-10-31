4165ef256c466f238188480f84ec87cfb82fa72d
Pierre <53476143+PPaillard@users.noreply.github.com>
2025-10-31 12:21:24 +0100

    Article

--- ArticleController.js
+++ ArticleController.js
@@ -1,2 +1,95 @@
+import database from "./database.js";
 
+const list = (req, res) => {
+  database
+    .query("select * from article")
+    .then(([articles]) => {
+      res.json(articles);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.sendStatus(500);
+    });
+};
 
+export const show = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  database
+    .query("SELECT * FROM article WHERE id=?", [id])
+    .then(([articles]) => {
+      if (articles[0]) {
+        //
+        res.json(articles[0]);
+      } else res.sendStatus(404);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.sendStatus(500);
+    });
+};
+
+export const insert = (req, res) => {
+  // verification, est-ce que body est invalide?
+  if (!req.body) {
+    return res.sendStatus(400);
+  }
+  const { name } = req.body;
+
+  database
+    .query("INSERT INTO article (name) VALUES (?)", [name])
+    .then(([result]) => {
+      res.status(201).json({
+        message: "Ajouté",
+        id: result.insertId,
+        name: name,
+      });
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+export const update = (req, res) => {
+  // verification, est-ce que body est invalide?
+  if (!req.body) {
+    return res.sendStatus(400);
+  }
+  const id = parseInt(req.params.id);
+  const { name } = req.body;
+  database
+    .query("UPDATE article SET name = ? WHERE id = ?", [name, id])
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.sendStatus(404);
+      } else {
+        res.json({
+          message: "Modifié",
+        });
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+export const remove = (req, res) => {
+  const id = parseInt(req.params.id);
+  database
+    .query("DELETE FROM article WHERE id = ?", [id])
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.sendStatus(404);
+      } else {
+        res.json({ message: `Article #${id} supprimé` });
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+export default list;
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 
-import database from "./database.js";
+import list, { insert, remove, show, update } from "./ArticleController.js";
+
 const app = express();
 
 app.use(express.json());
@@ -9,85 +10,19 @@ const serverPort = 3000;
 
 // GET /articles
 // /articles?limit=2
-app.get("/articles", (req, res) => {
-  // version destructurée
-  // database.query("SELECT * FROM article").then(([articles]) => {
-  database
-    .query("SELECT * FROM article")
-    .then((results) => {
-      console.log(results);
-      const [articles] = results;
-      res.json(articles);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.sendStatus(500);
-    });
-});
+app.get("/articles", list);
 
 // GET /articles/:id
-app.get("/articles/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-
-  database
-    .query("SELECT * FROM article WHERE id=?", [id])
-    .then((results) => {
-      console.log(results);
-      const [articles] = results;
-      if (articles.length > 0) {
-        res.json(articles[0]);
-      } else {
-        res.sendStatus(404);
-      }
-    })
-    .catch((err) => {
-      console.error(err);
-      res.sendStatus(500);
-    });
-});
+app.get("/articles/:id", show);
 
 // POST
-app.post("/articles", (req, res) => {
-  // verification, est-ce que body est invalide?
-  //if(req.body){
-  if (req.body == undefined) {
-    return res.sendStatus(400);
-  }
-  // On prend les données du POST
-  const article = req.body;
-  articles.push(article);
-  res.status(201).json({ message: "Ajouté", article: article });
-});
+app.post("/articles", insert);
 
 // PUT
-app.put("/articles/:id", (req, res) => {
-  // verification, est-ce que body est invalide?
-  //if(req.body){
-  if (req.body == undefined) {
-    return res.sendStatus(400);
-  }
-  const id = parseInt(req.params.id);
-  // On verifie si l’id existe
-  const index = articles.findIndex((t) => t.id === id);
-  console.log("Index : ", index, "\n");
-  if (index === -1) {
-    return res.sendStatus(404);
-  }
-  articles[index] = req.body;
-  res.json({ message: "Modifié", article: req.body });
-});
+app.put("/articles/:id", update);
 
 // DELETE
-app.delete("/articles/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-  // On verifie si l’id existe
-  const index = articles.findIndex((article) => article.id === id);
-  if (index === -1) {
-    return res.sendStatus(404);
-  }
-  articles.splice(index, 1);
-  res.json({ message: `article #${id} supprimé` });
-});
+app.delete("/articles/:id", remove);
 
 app.get("/", (req, res) => {
   console.log("J'ai une requête : ", req);
